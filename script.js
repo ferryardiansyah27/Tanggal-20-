645c8e3eee89234aebe016447a33ad30b00c02ad
Ardiansyah <ferry.123400067@student.itera.ac.id>
2026-01-20 14:28:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    const message = `
+Hai sayang ❤️  
+
+Tidak terasa kita sudah bersama sejauh ini.  
+Terima kasih sudah menemani hari-hariku,  
+mendengarkan ceritaku, dan tetap memilih aku setiap hari.  
+
+Semoga kita selalu saling menjaga, saling mencintai,  
+dan berjalan bersama sampai masa depan yang kita impikan 💍  
+
+Happy Anniversary, cintaku 🌹  
+Aku sayang kamu ❤️
+    `;
+
+    document.getElementById("message").innerText = message;
+}
